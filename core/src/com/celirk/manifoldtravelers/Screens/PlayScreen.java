4236af4a371e688566af32b6911c5f98a2a4e853
lydlid <lydlid01@gmail.com>
2021-05-24 17:59:50 +0800

    more playable

--- core/src/com/celirk/manifoldtravelers/Screens/PlayScreen.java
+++ core/src/com/celirk/manifoldtravelers/Screens/PlayScreen.java
@@ -1,20 +1,25 @@
 package com.celirk.manifoldtravelers.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.celirk.manifoldtravelers.ManifoldTravelers;
 import com.celirk.manifoldtravelers.Scenes.Hud;
+import com.celirk.manifoldtravelers.Sprites.Player;
+import com.celirk.manifoldtravelers.Utils.B2WorldCreator;
 
 public class PlayScreen implements Screen {
     private ManifoldTravelers game;
+
     private OrthographicCamera gamecam;
     private Viewport gamePort;
     private Hud hud;
@@ -23,11 +28,32 @@ public class PlayScreen implements Screen {
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
 
+    private World world;
+    private Box2DDebugRenderer b2dr;
+
+    private Player player;
+
     public PlayScreen(ManifoldTravelers game){
         this.game = game;
+
         gamecam = new OrthographicCamera();
-        gamePort = new FitViewport(ManifoldTravelers.V_WIDTH,ManifoldTravelers.V_HEIGHT,gamecam);
+
+        gamePort = new FitViewport(ManifoldTravelers.V_WIDTH / ManifoldTravelers.PPM,ManifoldTravelers.V_HEIGHT / ManifoldTravelers.PPM,gamecam);
+
         hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("l1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map, 1 / ManifoldTravelers.PPM);
+
+        gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
+        world = new World(new Vector2(0,-10),true);
+        b2dr = new Box2DDebugRenderer();
+
+        new B2WorldCreator(world, map);
+
+        player = new Player(world);
     }
     @Override
     public void show() {
@@ -36,11 +62,41 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
-    Gdx.gl.glClearColor(1,0,0,1);
-    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        update(delta);
 
-    game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
-    hud.stage.draw();
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
+
+        b2dr.render(world, gamecam.combined);
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+    }
+
+    public void handleInput(float delta) {
+        if(Gdx.input.isKeyJustPressed(Input.Keys.UP))
+            player.b2body.applyLinearImpulse(new Vector2(0,4f),player.b2body.getWorldCenter(),true);
+
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && player.b2body.getLinearVelocity().x <= 2)
+            player.b2body.applyLinearImpulse(new Vector2(0.1f,0),player.b2body.getWorldCenter(),true);
+
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && player.b2body.getLinearVelocity().x >= -2)
+            player.b2body.applyLinearImpulse(new Vector2(-0.1f,0),player.b2body.getWorldCenter(),true);
+    }
+
+    public void update(float delta) {
+        // dt := delta time
+        handleInput(delta);
+
+        world.step(1/60f, 6, 2);
+
+        gamecam.position.x = player.b2body.getPosition().x;
+        gamecam.position.y = player.b2body.getPosition().y;
+
+        gamecam.update();
+        renderer.setView(gamecam);
     }
 
     @Override
@@ -65,6 +121,10 @@ public class PlayScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        map.dispose();
+        renderer.dispose();
+        world.dispose();
+        b2dr.dispose();
+        hud.dispose();
     }
 }
