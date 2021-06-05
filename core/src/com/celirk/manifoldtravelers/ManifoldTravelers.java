1a47573ecc5ddfc49c690cbb92b2c99738ac5b6a
Lirkrid <lirkrid@ruc.edu.cn>
2021-06-05 14:58:44 +0800

    node js

--- core/src/com/celirk/manifoldtravelers/ManifoldTravelers.java
+++ core/src/com/celirk/manifoldtravelers/ManifoldTravelers.java
@@ -1,11 +1,23 @@
 package com.celirk.manifoldtravelers;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.celirk.manifoldtravelers.Screens.PlayScreen;
 import com.celirk.manifoldtravelers.Screens.MenuScreen;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+import netscape.javascript.JSObject;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class ManifoldTravelers extends Game {
+    //socket
+    private Socket socket;
+
+
     public static final int V_WIDTH = 400;
     public static final int V_HEIGHT = 225;
     // pixels per meter
@@ -34,8 +46,9 @@ public class ManifoldTravelers extends Game {
     public void create() {
         batch = new SpriteBatch();
         this.setScreen(new MenuScreen(this));
-
-    }
+        connectSocket();
+        configSocketEvents();
+}
 
     @Override
     public void render() {
@@ -46,4 +59,46 @@ public class ManifoldTravelers extends Game {
 //	public void dispose () {
 //		batch.dispose();
 //	}
+
+    public void connectSocket(){
+        try{
+            System.out.println("I'm here.");
+            socket = IO.socket("http://localhost:5432");
+            System.out.println(socket.connect());
+            System.out.println("I'm here too.");
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void configSocketEvents(){
+        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener(){
+            @Override
+            public void call(Object... args) {
+                Gdx.app.log("SocketIO","Connected");
+            }
+        }).on("socketID", new Emitter.Listener(){
+            @Override
+            public void call(Object... args) {
+                JSONObject data = (JSONObject) args[0];
+                try {
+                    String id = data.getString("id");
+                    Gdx.app.log("SocketIO", "My ID:" + id);
+                }catch(JSONException e){
+                    Gdx.app.log("SocketIO", "Error getting ID");
+                }
+            }
+        }).on("newPlayer", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                JSONObject data = (JSONObject) args[0];
+                try {
+                    String id = data.getString("id");
+                    Gdx.app.log("SocketIO", "New Player Connected:" + id);
+                }catch(JSONException e){
+                    Gdx.app.log("SocketIO", "Error getting New PlayerID");
+                }
+            }
+        });
+    }
 }
---
+++ server/index.js
@@ -0,0 +1,16 @@
+﻿var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(5432,function(){
+    console.log("Server is now running...");
+});
+
+io.on('connection',  function(socket){
+    console.log("Player Connected!");
+    socket.emit('socketID',{ id:socket.id });
+    socket.broadcast.emit('newPlayer',{  id:socket.id});
+    socket.on('disconnect',  function(){
+        console.log("Player Disconnected");
+    });
+});
