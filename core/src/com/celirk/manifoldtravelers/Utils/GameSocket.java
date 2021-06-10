8eeee1c303bd786e616749a8d474bd923b575a88
Cel@LAVIE <lydlid01@gmail.com>
2021-06-10 10:39:30 +0800

    sfx

--- core/src/com/celirk/manifoldtravelers/Utils/GameSocket.java
+++ core/src/com/celirk/manifoldtravelers/Utils/GameSocket.java
@@ -40,8 +40,7 @@ public class GameSocket {
 
     public void connectSocket(){
         try{
-            //socket = IO.socket("http://localhost:5432");
-            socket = IO.socket("http://localhost:5432");
+
             socket = IO.socket("http://localhost:5432");
             //socket = IO.socket("http://10.44.64.238:5432");
             socket.connect();
